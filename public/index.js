b8a037733d17fac82e888aa5e722a2b54f98549d
zacharyad <zacharyadcoding@gmail.com>
2024-06-18 13:14:54 -0400

    Feat: Button to clear all selected logos and offer good UX for seeing this

--- public/index.js
+++ public/index.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
+  let selectedLogos = [];
+  let countSelected = 0;
+
   const logoGrid = document.getElementById('logo-grid');
   const generatedUrlElement = document.getElementById('generated-url');
   const copyUrlButton = document.getElementById('copy-url-button');
-
-  let selectedLogos = [];
-  let countSelected = 0;
+  const clearUrlButton = document.getElementById('clear-url-button');
 
   // Fetch the list of images from the server
   fetch('/images-list')
@@ -35,14 +36,17 @@ document.addEventListener('DOMContentLoaded', function () {
         logoItem.addEventListener('click', () => {
           if (logoItem.classList.contains('selected')) {
             logoItem.classList.remove('selected');
+
             selectedLogos = selectedLogos.filter((item) => item !== logo);
             orderCount.style.opacity = 0;
             countSelected--;
-            reorderAllFlags(selectedLogos, logoItem.countFlag);
+            reorderAllFlags(logoItem.countFlag);
           } else {
             logoItem.classList.add('selected');
             selectedLogos.push(logo);
             orderCount.style.opacity = 100;
+            clearUrlButton.style.opacity = 100;
+
             ++countSelected;
             logoItem.countFlag = countSelected;
             orderCount.innerText = countSelected;
@@ -53,6 +57,8 @@ document.addEventListener('DOMContentLoaded', function () {
           if (generatedUrlElement.value.length === 30) {
             generatedUrlElement.value = '';
             copyUrlButton.disabled = true;
+            clearUrlButton.disabled = true;
+            clearUrlButton.style.opacity = 0;
           }
         });
 
@@ -62,7 +68,7 @@ document.addEventListener('DOMContentLoaded', function () {
             selectedLogos = selectedLogos.filter((item) => item !== logo);
             orderCount.style.opacity = 0;
             countSelected--;
-            reorderAllFlags(selectedLogos, logoItem.countFlag);
+            reorderAllFlags(logoItem.countFlag);
           } else {
             logoItem.classList.add('selected');
             selectedLogos.push(logo);
@@ -77,6 +83,7 @@ document.addEventListener('DOMContentLoaded', function () {
           if (generatedUrlElement.value.length === 30) {
             generatedUrlElement.value = '';
             copyUrlButton.disabled = true;
+            clearUrlButton.disabled = true;
           }
         });
 
@@ -90,13 +97,28 @@ document.addEventListener('DOMContentLoaded', function () {
       copyUrlButton.innerText = 'Copy URL';
       copyUrlButton.style.backgroundColor = '#007aff';
     }
+    if (clearUrlButton.innerText === 'Cleared') {
+      clearUrlButton.innerText = 'Clear Selections';
+      clearUrlButton.style.backgroundColor = '#ff948b';
+    }
+
     copyUrlButton.disabled = false;
+    clearUrlButton.disabled = false;
+
     const url = `https://www.stackimages.xyz/l/${selectedLogos
       .map((item) => item.split('/').pop().split('.')[0])
       .join('-')}`;
 
     generatedUrlElement.value = url;
   }
+
+  // eventListerns
+
+  clearUrlButton.addEventListener('click', () => {
+    generatedUrlElement.value = '';
+
+    clearAllSelections();
+  });
 
   copyUrlButton.addEventListener('click', () => {
     const url = generatedUrlElement.value;
@@ -111,30 +133,60 @@ document.addEventListener('DOMContentLoaded', function () {
         console.error('Failed to copy URL: ', err);
       });
   });
-});
 
-function reorderAllFlags(selectedLogos, numberDeleted) {
-  console.log('numberDeleted: ', numberDeleted);
+  function reorderAllFlags(numberDeleted) {
+    console.log('numberDeleted: ', numberDeleted);
 
-  let allLogos = [...document.querySelectorAll('.logoimage')]
-    .filter((elem) => {
-      return selectedLogos.includes(elem.logoName);
-    })
-    .map((elem) => {
-      return {
-        parent: elem,
-        flag: elem.children[0],
-        prevCount: elem.countFlag - 1,
-      };
+    let allLogos = [...document.querySelectorAll('.logoimage')]
+      .filter((elem) => {
+        return selectedLogos.includes(elem.logoName);
+      })
+      .map((elem) => {
+        return {
+          parent: elem,
+          flag: elem.children[0],
+          prevCount: elem.countFlag - 1,
+        };
+      });
+
+    countSelected = allLogos.length;
+
+    allLogos.forEach((elem) => {
+      if (elem.prevCount >= numberDeleted) {
+        elem.flag.innerText = elem.prevCount;
+        elem.parent.countFlag = elem.prevCount;
+      }
     });
+  }
 
-  countSelected = allLogos.length - 1;
+  function clearAllSelections() {
+    let allLogos = [...document.querySelectorAll('.logoimage')]
+      .filter((elem) => {
+        return selectedLogos.includes(elem.logoName);
+      })
+      .forEach((elem) => {
+        resetFlagsOnSelectedElems(elem);
+      });
 
-  allLogos.forEach((elem) => {
-    if (elem.prevCount >= numberDeleted) {
-      elem.flag.innerText = elem.prevCount;
-      elem.flag.countFlag = elem.prevCount;
-      elem.parent.countFlag = elem.prevCount;
-    }
-  });
-}
+    resetGlobalVars();
+
+    clearUrlButton.classList.toggle('fade');
+    clearUrlButton.style.opacity = 0;
+  }
+
+  function resetGlobalVars() {
+    selectedLogos = [];
+    countSelected = selectedLogos.length;
+  }
+
+  function resetFlagsOnSelectedElems(elem) {
+    let flagElem = elem.children[0];
+
+    elem.classList.remove('selected');
+    flagElem.innerText = 0;
+    flagElem.style.opacity = 0;
+    generatedUrlElement.disabled = true;
+    clearUrlButton.innerText = 'Cleared';
+    elem.countFlag = 0;
+  }
+});
